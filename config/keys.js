8e0b2275bb1775270b02e7869fe6a27e90d595c4
Dorjan Hysa <dorjan@MBP-di-Dorjan.lan>
2020-07-10 20:38:20 +0200

    modifica ampqURI

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,8 @@ module.exports = {
 
     mongoDB : 'mongodb+srv://Marco:Marco.141995@progetto-lemgb.mongodb.net/test?retryWrites=true&w=majority',
     
-    ampqURI: 'amqp://localhost:5672/',
+    ampqURI: 'amqp://fnqvktyp:lerr2SFOaWU-rlsurI4NH0XL5ohsg-JL@roedeer.rmq.cloudamqp.com/fnqvktyp', 
+    //ampqURI: 'amqp://localhost:5672/',
     
     
     google: {
